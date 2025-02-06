73db9d4d964065390a5a25e2640827f8648c95c6
struhanets <posseydon87@gmail.com>
2025-02-06 18:43:35 +0100

    Created schemas for Temperature model

--- temperature/schemas.py
+++ temperature/schemas.py
@@ -3,26 +3,6 @@ from datetime import datetime
 from pydantic import BaseModel
 
 
-class CityBase(BaseModel):
-    name: str
-    additional_info: str
-
-
-class CityCreate(CityBase):
-    pass
-
-
-class CityUpdate(CityCreate):
-    pass
-
-
-class City(CityBase):
-    id: int
-
-    class Config:
-        from_attributes = True
-
-
 class TemperatureBase(BaseModel):
     date_time: datetime
     temperature: int
