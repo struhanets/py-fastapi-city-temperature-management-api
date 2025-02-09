4725b5a2887c71b3af24ce3ad7ab67f08b4e5fe3
struhanets <posseydon87@gmail.com>
2025-02-09 08:42:01 +0100

    Add router for City model with URL endpoints

---
+++ city/router.py
@@ -0,0 +1,44 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from city import schemas, crud
+from dependencies import get_db
+
+router = APIRouter()
+
+
+@router.get("cities/", response_model=list[schemas.City])
+async def read_all_cities(db: AsyncSession = Depends(get_db)):
+    return await crud.get_cities(db=db)
+
+
+@router.post("cities/", response_model=schemas.CityCreate)
+async def new_city(city: schemas.CityCreate, db: AsyncSession = Depends(get_db)):
+    return await crud.create_city(db=db, city=city)
+
+
+@router.get("cities/{city_id}", response_model=schemas.City)
+async def read_city(city_id: int, db: AsyncSession = Depends(get_db)):
+    db_city = await crud.get_city_by_id(db=db, city_id=city_id)
+    if not db_city:
+        raise HTTPException(status_code=404, detail="City not found")
+
+    return db_city
+
+
+@router.put("cities/{city_id}", response_model=schemas.City)
+async def update_city(city_id: int, city: schemas.CityCreate, db: AsyncSession = Depends(get_db)):
+    updated_city = await crud.update_city(db=db, city_id=city_id, city=city)
+    if not updated_city:
+        raise HTTPException(status_code=404, detail="City not found")
+
+    return updated_city
+
+
+@router.delete("cities/{city_id}", response_model=schemas.City)
+async def delete_city(city_id: int, db: AsyncSession = Depends(get_db)):
+    db_city = await crud.delete_city(db=db, city_id=city_id)
+    if not db_city:
+        raise HTTPException(status_code=404, detail="City not found")
+
+    return db_city
